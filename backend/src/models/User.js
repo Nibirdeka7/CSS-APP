89c99470850c202c694d80b2106a6bfd4192eab1
kallul <gogoikallul33@gmail.com>
2026-01-23 01:37:20 +0530

    google auth backend logic added

---
+++ backend/src/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^[a-z0-9_]+@cse\.nits\.ac\.in$/,
+      "(Bhai/Behen) only CSE students allowed he ",
+    ],
+  },
+
+  googleId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+
+  points: {
+    type: Number,
+    default: 1000,
+  },
+
+  role: {
+    type: String,
+    enum: ["USER", "ADMIN"],
+    default: "USER",
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
