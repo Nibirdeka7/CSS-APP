6605d11df17ff0e201bceb8a03ef4505569b7f2f
Nibir <nibirdeka70@gmail.com>
2026-01-24 23:18:53 +0530

    fix: update CORS configuration for improved security and add Vercel configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,10 +16,7 @@ const app = express();
 // Middleware
 job.start();
 app.use(cors({
-  origin: ["http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true,
-
+  origin: '*',
 }));
 
 app.use(express.json());
@@ -41,8 +38,8 @@ app.get("/", (req, res) => {
 // Start server
 connectDB();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+// app.listen(PORT, () =>
+//   console.log(`Server running on http://localhost:${PORT}`),
+// );
 
 module.exports = app;
