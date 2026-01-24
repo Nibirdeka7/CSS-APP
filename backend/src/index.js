f90b43802cddd74104d190c79415072acab4611b
Nibir <nibirdeka70@gmail.com>
2026-01-24 23:39:07 +0530

    refactor: simplify CORS middleware configuration and enable default settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,9 +15,7 @@ const app = express();
 
 // Middleware
 job.start();
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors());
 
 app.use(express.json());
 
@@ -38,8 +36,8 @@ app.get("/", (req, res) => {
 // Start server
 connectDB();
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () =>
-//   console.log(`Server running on http://localhost:${PORT}`),
-// );
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
 
 module.exports = app;
