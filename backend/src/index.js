87c12af0b4a21696339572b9a396796f928be275
Nibir <nibirdeka70@gmail.com>
2026-01-24 21:53:56 +0530

    fixing for backend deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,8 +32,10 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-connectDB();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+// connectDB();
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`Server running on http://localhost:${PORT}`),
+// );
+
+module.exports = app;
