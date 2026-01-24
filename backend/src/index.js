73a5125272e873354586d57887980d2d53cc624b
Nibir <nibirdeka70@gmail.com>
2026-01-24 23:43:39 +0530

    refactor: update server entry point and adjust Vercel configuration for API routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,9 +35,9 @@ app.get("/", (req, res) => {
 
 // Start server
 connectDB();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`Server running on http://localhost:${PORT}`),
+// );
 
 module.exports = app;
