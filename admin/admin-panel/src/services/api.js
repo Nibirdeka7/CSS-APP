2f0fc40c5efab750d47447d0e58f9ce739e3e477
Nibir <nibirdeka70@gmail.com>
2026-01-27 20:23:03 +0530

    fix: Update API base URL to use environment variable for better configuration management

--- admin/admin-panel/src/services/api.js
+++ admin/admin-panel/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://css-app-mfog.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL,
   // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
